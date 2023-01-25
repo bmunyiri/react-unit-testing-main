fe985b986f2b94bd624f4aaf917fb46a60a7e07b
Bernard Munyiri <bernard.munyiri@gmail.com>
2023-01-25 14:52:15 +0300

    React-Unit-Testing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import Comments from "./components/Comments";
+
+function App() {
+  return (
+    <div className="App">
+ <Counter /> 
+      <Comments />
+    </div>
+  );
+}
+
+export default App;
